d57610d0911ea39e421da586e5acf49b9e6ba2c5
Prateek Poudel <prateekpoudel61@gmail.com>
2024-06-14 23:09:15 +0545

    Done

--- src/camera.h
+++ src/camera.h
@@ -14,6 +14,14 @@ public:
     int samples_per_pixel = 10; // Number of samples per pixel
     int max_depth = 10; // Maximum number of bounces for a ray
 
+    double vfov = 90; // Vertical field of view in degrees
+    point3 lookfrom; // Camera location
+    point3 lookat; // Camera target
+    vec3 vup; // Camera up vector
+
+    double defocus_angle = 0; // Variation in the focus point
+    double focus_dist = 10; // Distance from camera lookfrom point to plane of perfect focus
+
     void render(const hittable& world);
     void render_threaded(const hittable& world);
 private:
@@ -23,6 +31,16 @@ private:
     point3 pixel00_loc; // Location of the upper left pixel
     vec3 pixel_delta_u; // Horizontal delta from pixel to pixel
     vec3 pixel_delta_v; // Vertical delta from pixel to pixel
+    vec3 u, v, w;       // Camera basis vectors
+    vec3 defocus_disk_u; // Defocus disk horizontal radius
+    vec3 defocus_disk_v; // Defocus disk vertical radius
+
+    point3 defocus_disk_sample() const
+    {
+        // Returns a random point on the camera defocus disk.
+        auto p = random_in_unit_disk();
+        return center + (p[0] * defocus_disk_u) + (p[1] * defocus_disk_v);
+    }
 
     void initialize();
     color ray_color(const ray& r, int depth, const hittable& world) const;
@@ -52,7 +70,7 @@ inline void camera::render_threaded(const hittable& world)
 
             for (int j = start; j < end; j++)
             {
-                std::clog << "\rScanlines remaining: " << (end - j) << ' ' << std::flush;
+                std::clog << "\rScanlines remaining: " << (end - j) << "                      " << std::flush;
                 for (int i = 0; i < image_width; i++)
                 {
                     color pixel_color(0, 0, 0);
@@ -81,7 +99,7 @@ inline void camera::render_threaded(const hittable& world)
             write_color(std::cout, colors[i][j] * pixel_sample_scale);
         }
     }
-    std::clog << "\rDone.                 \n";
+    std::clog << "\rDone.                                 \n";
 }
 
 inline void camera::render(const hittable &world)
@@ -118,24 +136,35 @@ inline void camera::initialize()
 
     pixel_sample_scale = 1.0 / samples_per_pixel;
 
-    center = point3(0, 0, 0);
+    center = lookfrom;
 
     // Determine viewport dimensions
-    auto focal_length = 1.0;
-    auto viewport_height = 2.0;
+    auto theta = degrees_to_radians(vfov);
+    auto h = tan(theta / 2);
+    auto viewport_height = 2 * h * focus_dist;
     auto viewport_width = viewport_height * (double(image_width) / image_height);
 
+    // Calculate the u, v, w unit vectors from the camera coordinates
+    w = unit_vector(lookfrom - lookat);
+    u = unit_vector(cross(vup, w));
+    v = cross(w, u);
+
     // Calculate the vectors across the horizontal and down the vertical dimensions of the viewport
-    auto viewport_u = vec3(viewport_width, 0, 0);
-    auto viewport_v = vec3(0, -viewport_height, 0);
+    vec3 viewport_u = viewport_width * u; // Vector across viewport horizontal edge
+    vec3 viewport_v = viewport_height * -v; // Vector down viewport vertical edge
 
     // Calculate the horizontal and vertical deltas from pixel to pixel
     pixel_delta_u = viewport_u / image_width;
     pixel_delta_v = viewport_v / image_height;
 
     // Calculate the location of the upper left pixel
-    auto viewport_upper_left = center - vec3(0, 0, focal_length) - viewport_u / 2 - viewport_v / 2;
+    auto viewport_upper_left = center - (focus_dist * w) - 0.5 * (viewport_u + viewport_v);
     pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+
+    // Calculate the camera defocus disk
+    auto defocus_radius = focus_dist * tan(degrees_to_radians(defocus_angle / 2));
+    defocus_disk_u = u * defocus_radius;
+    defocus_disk_v = v * defocus_radius;
 }
 
 color camera::ray_color(const ray &r, int depth, const hittable &world) const
@@ -167,7 +196,7 @@ inline ray camera::get_ray(int i, int j) const
     auto offset = sample_square();
     auto pixel_sample = pixel00_loc + (i + offset.x()) * pixel_delta_u + (j + offset.y()) * pixel_delta_v;
 
-    auto ray_origin = center;
+    auto ray_origin = (defocus_angle <= 0) ? center : defocus_disk_sample();
     auto ray_direction = pixel_sample - ray_origin;
 
     return ray(ray_origin, ray_direction);
